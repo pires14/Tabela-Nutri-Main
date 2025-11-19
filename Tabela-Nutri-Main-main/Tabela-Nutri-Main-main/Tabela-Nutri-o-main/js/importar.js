69bcf4b7c62993c5174d2242d7e1971ddda786a1
pires14 <joao.bernardo.pires@escola.pr.gov.br>
2025-11-19 09:48:11 -0300

    Add files via upload

---
+++ Tabela-Nutri-Main-main/Tabela-Nutri-Main-main/Tabela-Nutri-o-main/js/importar.js
@@ -0,0 +1,6 @@
+let botaoImportar = document.querySelector("#importar-paciente");
+
+botaoImportar.addEventListener('click', function(){
+   //Acessando a requisição
+   let xhr = new XMLHttpRequest()
+});
